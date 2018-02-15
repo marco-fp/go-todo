4043a9428efbd726456a35f9c122df4ba4bdb48c
Marco <mfernandezpranno@gmail.com>
2018-02-15 23:12:01 +0100

    Fixed some bugs due to sintax. CRUD implemented completely.

--- main.go
+++ main.go
@@ -29,7 +29,6 @@ func init() {
   if err != nil {
     panic("Error connecting to database")
   }
-  defer db.Close()
 
   // Migrate the schema
   db.AutoMigrate(&Todo{})
@@ -40,33 +39,83 @@ func main() {
 
   v1 := router.Group("/api/v1/todo")
   {
-    v1.GET("/ping", pong)
     v1.POST("/", createTodo)
-    // v1.GET("/", getTodos)
-    // v1.GET("/:id", getTodo)
-    // v1.PUT("/:id", updateTodo)
-    // v1.DELETE("/:id", deleteTodo)
+    v1.GET("/", getTodos)
+    v1.GET("/:id", getTodo)
+    v1.PUT("/:id", updateTodo)
+    v1.DELETE("/:id", deleteTodo)
   }
 
   router.Run(":8080")
-}
-
-func pong(c *gin.Context) {
-  db.Create(&Todo{Title: "Test", Completed: "true"})
-
-  c.JSON(http.StatusOK, gin.H{
-    "message": "pong",
-  })
 }
 
 func createTodo(c *gin.Context) {
   var json TodoJSON
 
   if err := c.ShouldBindJSON(&json); err == nil {
-    db.Create(Todo{Title: json.Title, Completed: json.Completed})
+    db.Create(&Todo{Title: json.Title, Completed: json.Completed})
 
     c.JSON(http.StatusOK, gin.H{"status": "Todo created"})
   } else {
     c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
   }
+}
+
+func getTodos(c *gin.Context) {
+  var todos []Todo
+
+  db.Find(&todos)
+
+  if len(todos) <= 0 {
+    c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todos found."})
+  } else {
+    c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": todos})
+  }
+}
+
+func getTodo(c *gin.Context) {
+  var todo Todo
+  todoID := c.Param("id")
+  db.Find(&todo, todoID)
+  if todo.ID == 0 {
+    c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "Todo not found."})
+  } else {
+    c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": todo})
+  }
+}
+
+func updateTodo(c *gin.Context) {
+  var todo Todo
+  todoID := c.Param("id")
+
+  db.Find(&todo, todoID)
+
+  if todo.ID == 0 {
+    c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "Todo not found."})
+    return
+  }
+
+  var json TodoJSON
+  if err := c.ShouldBindJSON(&json); err == nil {
+    db.Model(&todo).Updates(Todo{Title: json.Title, Completed: json.Completed})
+    c.JSON(http.StatusOK, gin.H{"status": "Todo updated"})
+  } else {
+    c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+  }
+}
+
+func deleteTodo(c *gin.Context) {
+  var todo Todo
+  todoID := c.Param("id")
+
+  db.First(&todo, todoID)
+
+  if todo.ID == 0 {
+    c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "Todo not found."})
+    return
+  }
+
+  db.Delete(&todo)
+
+  c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Todo deleted."})
 }
